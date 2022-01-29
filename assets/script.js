4787d683f8b3b9a7a6b27f9bb20a6c9a947e259e
unknown <jam319@protonmail.com>
2022-01-29 12:45:40 -0500

    added functionality to increse/decrease buttons

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+let decreaseBtn = document.querySelector('#quantity-down')
+let increaseBtn = document.querySelector('#quantity-up')
+let removeBtn = document.querySelector('.remove')
+
+function updateQuantity(displayQuantity){
+    let quantity = document.querySelector('.total-quantity')
+    quantity.innerHTML = displayQuantity
+}
+
+let quantity = 1
+
+decreaseBtn.addEventListener('click', function(e){
+    if(quantity > 0){
+        quantity--
+    } else {
+        window.alert("Nothing in cart")
+    }
+    updateQuantity(`Quantity: ${quantity}`)
+})
+increaseBtn.addEventListener('click', function(e){
+        quantity++
+    updateQuantity(`Quantity: ${quantity}`)
+})
+
